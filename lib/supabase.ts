f4df3b4725731789625d009451af9271d238138c
maifree123 <71332774+maifree123@users.noreply.github.com>
2025-03-31 03:06:29 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Supabase 项目的 URL 和公共 API 密钥
-const SUPABASE_URL = 'https://uhqmcsokbrqdhzgcxdrz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVocW1jc29rYnJxZGh6Z2N4ZHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczODgxNjMsImV4cCI6MjA1Mjk2NDE2M30.JjaOgRDSWZ_gmvyjRwQPNqjD8Qrt9wYob9FmuLRR0S4';
+const SUPABASE_URL = '';
+const SUPABASE_ANON_KEY = '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
